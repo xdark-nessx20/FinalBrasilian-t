8f340675efadb6c69787ef8c9caf0eee96e76f50
Luis Saumeth <luijasaumethanaya@gmail.com>
2025-11-07 09:03:59 -0500

    minor changes in userDTO

--- src/main/java/co/edu/unimagdalena/finalbrasiliant/api/dto/UserDTO.java
+++ src/main/java/co/edu/unimagdalena/finalbrasiliant/api/dto/UserDTO.java
@@ -2,17 +2,18 @@ package co.edu.unimagdalena.finalbrasiliant.api.dto;
 
 import java.io.Serializable;
 
+import co.edu.unimagdalena.finalbrasiliant.domain.enums.Role;
 import jakarta.annotation.Nonnull;
 
 public class UserDTO {
 	public record userCreateRequest(@Nonnull String userName, @Nonnull String email, 
-			@Nonnull String phone, @Nonnull String role, @Nonnull String passwordHash) implements Serializable{};
+			@Nonnull String phone, @Nonnull Role role, @Nonnull String passwordHash) implements Serializable{};
 			
 	public record userUpdateRequest(String userName, String email, 
-			String phone, String role, 
+			String phone, Role role, 
 			Boolean status, String passwordHash) implements Serializable{};
 	
 	public record userResponse(String userName, String email, 
-			String phone, String role, 
+			String phone, Role role, 
 			Boolean status, String passwordHash) implements Serializable{};
 }
